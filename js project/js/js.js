f9fa6abdbbe34545baa7d1862370c8b56b2ee934
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2021-02-23 16:22:52 +0200

    update

---
+++ js project/js/js.js
@@ -0,0 +1,19 @@
+//get the amount of documents
+// provide discount based on the amount of documents
+//display the discount
+
+alert ("How are you");
+var name = prompt ("What is your name");
+
+var amountOfdocuments= prompt("how many document do you want?")
+if (amountOfdocuments  < 10 ) {
+    alert(" input a number more than 10");
+}
+
+        else if {
+
+
+            alert("Hooray you earned 10% discount! ");
+
+
+        }
