7ddad6932048c35be2ee0ba2da5984ef279ba719
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2021-02-24 17:07:20 +0200

    up nto date

---
+++ lab05looping/looping.js
@@ -0,0 +1,17 @@
+var ask = prompt ("how many documents you wanted to translate? it should be <5");
+function display(){
+for (i = 0; i < 99999 ; i++) {
+    ask = prompt ("how many documents you wanted to translate? it should be <5");
+if (ask > 5) {
+        break; 
+    }}}
+display()
+var question = prompt ("Are you sure?") 
+    if (question =="yes"){
+        alert("then we will contact you very soon");
+        for(var i=0; i < ask; i++){
+            document.write("<img src=https://static9.depositphotos.com/1007989/1112/i/600/depositphotos_11129511-stock-photo-number-5.jpg''  />")
+        }
+    }else{
+        alert("Sorry we can not help you");
+    }
